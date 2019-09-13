7c25c3d8e76dab455a239795a9c5a22950de5bfa
Dominique Skaf <doskaf@yahoo.com>
2019-09-13 22:47:27 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,5 +1,11 @@
 def prime?(integer)
-  if integer == (Integer) * (Integer)
+  if integer / 2 == Integer
+    false
+  elsif integer / 3 == Integer
+    false
+  elsif integer / 5 == Integer
+    false
+  elsif integer / 7 == Integer
     false
   else
     true
