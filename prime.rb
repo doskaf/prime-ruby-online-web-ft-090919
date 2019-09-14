bbf6f019dd3d614ebd6a029a795550605e1b8349
Dominique Skaf <doskaf@yahoo.com>
2019-09-14 00:09:53 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,15 +1,7 @@
+def sieve
+  
+end
+
 def prime?(integer)
-  if integer / 2.0 == Integer
-    false
-  elsif integer / 3.0 == Integer
-    false
-  elsif integer / 5.0 == Integer
-    false
-  elsif integer / 7.0 == Integer
-    false
-  elsif integer < 2.0
-    false
-  else
-    true
-  end
+ 
 end
